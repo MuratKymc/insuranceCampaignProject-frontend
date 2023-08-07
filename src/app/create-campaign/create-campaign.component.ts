c58e972d04930066d823a3810ec518ce4d2343af
Murat <murat.kymc.16@gmail.com>
2023-08-07 22:55:25 +0300

    validations added and design updated

--- src/app/create-campaign/create-campaign.component.ts
+++ src/app/create-campaign/create-campaign.component.ts
@@ -2,13 +2,14 @@ import { Component, NgModule, OnInit } from '@angular/core';
 import { CampaignService } from '../campaign.service';
 import { Router } from '@angular/router';
 import { Campaign } from '../campaign';
+import { MinLengthValidator, MinValidator } from '@angular/forms';
 
 @Component({
   selector: 'app-create-campaign',
   templateUrl: './create-campaign.component.html',
   styleUrls: ['./create-campaign.component.css']
 })
-export class CreateCampaignComponent implements OnInit{
+export class CreateCampaignComponent implements OnInit {
 
 
   campaign: Campaign = new Campaign;
@@ -16,14 +17,14 @@ export class CreateCampaignComponent implements OnInit{
   constructor(
     private campaignService: CampaignService,
     private router: Router,
-    ){}
+  ) { }
 
 
   ngOnInit() {
   }
 
-  saveCampaign(){
-    this.campaignService.createCampaign(this.campaign).subscribe( data => {
+  saveCampaign() {
+    this.campaignService.createCampaign(this.campaign).subscribe(data => {
       console.log(data);
     },
       error => console.log(error)
@@ -31,14 +32,39 @@ export class CreateCampaignComponent implements OnInit{
   }
 
 
-  goToCampaignList(){
+  goToCampaignList() {
     this.router.navigate(['/campaigns'])
   }
 
-  onSubmit(){
+  onSubmit() {
     console.log(this.campaign);
     this.saveCampaign();
     this.goToCampaignList();
+  }
+
+
+  getValidationErrors(state: any, minlength: any) {
+    let ctrlName: string = state.name;
+    let messages: string[] = [];
+    
+
+    if (state.errors) {
+      for (let errorName in state.errors) {
+        switch (errorName) {
+          case "required":
+            messages.push(`Cannot be left blank.`);
+            break;
+          case "minlength":
+            messages.push(`At least `+ minlength + ` characters.`);
+            break;
+          case "pattern":
+            messages.push(`Invalid character. `);
+            break;
+        }
+      }
+
+    }
+    return messages;
   }
 
 
